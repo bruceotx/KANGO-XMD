5f352f1ba66a223a4aaf86a8505d75ea6a644ad9
bruceotx <feranmiakinro51@gmail.com>
2025-05-13 17:06:30 +0100

    Update settings.js
    ../

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-/*
+ /*
   project_name : 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 Bot
   Version : 2.4.0
   Owner : Hector Manuel
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~fRtghJqY#EziwlrZP6xNwaj8sLPhj4dEtMEenNEk40KiCXq5PEWs' 
 //Enter your 𝙆𝘼𝙉𝙂𝙊-𝙓𝙈𝘿 session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
